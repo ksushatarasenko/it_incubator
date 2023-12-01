42af181f1307986ab24f611d96d91e3a43a4b3a1
Ваше Имя <ksushatarasenko@gmail.com>
2023-12-01 16:46:01 +0100

    add array

---
+++ freeCodeCamp/lesson1_10/lesson1-10.js
@@ -0,0 +1,14 @@
+// 1) Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+// Создайте две новые строковые переменные: myFirstName и myLastName и присвойте им значения вашего имени и фамилии соответственно.
+var myFirstName = 'Oksana';
+var myLastName = 'Tarasenko';
+
+// Когда переменные JavaScript объявлены, они имеют начальное значение неопределенное(undefined). Если вы выполните математическую операцию с неопределенной переменной, ваш результат будет NaN, что означает «Не число». Если вы объедините строку с неопределенной переменной, вы получите строку неопределенного значения.
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = 'I am a';
+
+// 
