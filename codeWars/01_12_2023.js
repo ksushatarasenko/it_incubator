4c3174a5e78e12ab205c239c5ee920ba25caf49d
Ваше Имя <ksushatarasenko@gmail.com>
2023-12-01 18:35:35 +0100

    codeWars new

--- codeWars/01_12_2023.js
+++ codeWars/01_12_2023.js
@@ -26,4 +26,59 @@ console.log(ageVisiting(40));  // Вывод: "27-66"
 console.log(ageVisiting(10));  // Вывод: "9-13"
 console.log(ageVisiting(14));  // Вывод: "9-13"
 
+// =========================
+// Что, если нам нужно добавить длину слов, разделенных пробелом, в конце того же слова и вернуть ее в виде массива?
+// "apple ban" --> ["apple 5", "ban 3"]
 
+
+
+function addStr(str){
+    const worlds = str.split(' ');
+    const result = worlds.map((elem) => {
+        return `${elem} ${elem.length}`;
+    })
+    return result;
+}
+const inputString = "apple ban";
+const resultArray = addStr(inputString);
+console.log(resultArray);  
+// =================================
+// ((P-1)! + 1) / (P * P)
+// должен дать целое число.
+// Ваша задача — создать функцию, которая возвращает true, если заданное число является простым числом Вильсона.
+function isWilsonPrime(p) {
+    function factorialMod(p) {
+        let result = 1;
+        for (let i = 1; i <= p - 1; i++) {
+            result = (result * i) % (p * p);
+        }
+        return result;
+    }
+
+    return (factorialMod(p) + 1) % (p * p) === 0;
+}
+
+// Пример использования
+console.log(isWilsonPrime(5));  // Вывод: true, так как 4! + 1 = 25, и 5 * 5 = 25
+console.log(isWilsonPrime(6));  // Вывод: false
+
+// ======================================================
+// Вы забыли посчитать количество тостов, которые вы туда положили, вы не знаете, положили ли вы в тостеры ровно шесть тостов.
+// Определите функцию, которая подсчитывает, сколько еще (или меньше) кусков тостов вам понадобится в тостерах. Даже если вам нужно больше или меньше, число все равно будет положительным, а не отрицательным.
+// Примеры:
+// Вы должны вернуть количество тостов, которое вам нужно положить (или вынуть). В случае, если 5вы все еще можете положить 1тост:
+
+
+function sixToast(num) {
+    if(num < 6){
+        return 6 - num;
+    } else if (num > 6){
+        return num - 6;
+    } else {
+        return 0;
+    }
+  }
+
+  console.log(sixToast(6));
+  console.log(sixToast(17));
+  console.log(sixToast(3));
