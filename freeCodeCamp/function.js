cd1291282981f5d7b5e2ee44b24248400bb786a7
Oksana Tarasenko <120595084+ksushatarasenko@users.noreply.github.com>
2023-12-02 07:05:00 +0100

    new

--- freeCodeCamp/function.js
+++ freeCodeCamp/function.js
@@ -7,3 +7,22 @@ function timesFive(num){
 }
 timesFive(5);
 // =============================
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  
+const outerWear = 'sweater';
+  
+  return outerWear;
+}
+
+myOutfit();
+// ============================
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
