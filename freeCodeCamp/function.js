0a874492e2e9c7a86575c91b6ebf9dcba47ca733
Oksana Tarasenko <120595084+ksushatarasenko@users.noreply.github.com>
2023-12-02 06:56:29 +0100

    add new 02_12_23

---
+++ freeCodeCamp/function.js
@@ -0,0 +1,9 @@
+function functionWithArgs(num1, num2){
+    console.log(num1 + num2);
+  }
+//   ===========================
+function timesFive(num){
+  return num * 5
+}
+timesFive(5);
+// =============================
