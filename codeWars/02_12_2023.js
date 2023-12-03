8ada0fefe36df7c8a6e2b2db3f8558e4b5f3cbdd
Ваше Имя <ksushatarasenko@gmail.com>
2023-12-03 18:51:52 +0100

    new

--- codeWars/02_12_2023.js
+++ codeWars/02_12_2023.js
@@ -47,3 +47,26 @@ function ifChuckSaysSo(){
 }
 
 console.log(ifChuckSaysSo());
+// ====================
+function nextInLine(arr, item) {
+  arr.push(item);
+  item = arr.shift();
+  
+  return item;
+  
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+// =======================
+function trueOrFalse(wasThatTrue) {
+  if(wasThatTrue){
+    return trueOrFalse;
+  }
+  return false;
+}
