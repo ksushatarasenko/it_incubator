b2100a7ea125a5686900ed546f3b5662bb3ed8a2
Ваше Имя <ksushatarasenko@gmail.com>
2023-12-05 05:48:03 +0100

    new

--- freeCodeCamp/05_12_23.js
+++ freeCodeCamp/05_12_23.js
@@ -24,3 +24,22 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+function testSize(num) {
+  if (num <5){
+    return 'Tiny'
+  } else if (num < 10){
+    return 'Small'
+  } else if (num < 15){
+    return 'Medium'
+  } else if (num < 20){
+    return 'Large'
+  } else if( num >= 20){
+    return 'Huge'
+  } else{
+    return "Change Me";
+  }
+  
+}
+
+testSize(7);
