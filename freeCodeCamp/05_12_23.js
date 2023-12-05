959f9f1fee53d9ee1324ca02fd4a97c8a5972204
Ваше Имя <ksushatarasenko@gmail.com>
2023-12-05 06:21:46 +0100

    new

--- freeCodeCamp/05_12_23.js
+++ freeCodeCamp/05_12_23.js
@@ -43,3 +43,47 @@ function testSize(num) {
 }
 
 testSize(7);
+// Вашей функции будут переданы аргументы par и Strokes. Верните правильную строку в соответствии с этой таблицей, в которой штрихи перечислены в порядке приоритета; сверху (самый высокий) вниз (самый низкий):
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  
+  if (strokes === 1) {
+    return names[0]; 
+  } else if (strokes <= par - 2) {
+    return names[1]; 
+  } else if (strokes === par - 1) {
+    return names[2]; 
+  } else if (strokes === par) {
+    return names[3]; 
+  } else if (strokes === par + 1) {
+    return names[4]; 
+  } else if (strokes === par + 2) {
+    return names[5]; 
+  } else {
+    return names[6]; 
+  }
+} 
+
+golfScore(5, 4);
+//  SWITH
+function caseInSwitch(val) {
+  let answer = "";
+  switch(val) {
+    case '1':
+      return answer = 'alpha';
+      break;
+    case '2':
+      return answer = 'beta';
+      break;
+    case '3':
+      return answer = 'gamma';
+      break;
+    case '4':
+      return answer = 'delta';
+      break;
+    
+  }
+  return answer
+}
+caseInSwitch(1);
