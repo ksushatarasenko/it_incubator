3eab5d9c991275ab6e178079158b7f72810a9874
Oksana <ksushatarasenko@gmail.com>
2023-12-11 06:31:39 +0100

    new

--- freeCodeCamp/11_12_23.js
+++ freeCodeCamp/11_12_23.js
@@ -40,5 +40,43 @@ function sequentialSizes(val) {
         }
     return answer;
   }
+// =============
+function chainToSwitch(val) {
+    let answer = "";
+    switch(val){
+        case 'bob':
+            return "Matley";
+            break;
+        case 42:
+            return 'The Answer';
+            break;
+        case 1:
+            return "There is no #1";
+            break;
+        case 99:
+            return "Missed me by this much!";
+            break;
+        case 7:
+            return "Ate Nine";
+            break;
+    }
+  
+    if (val === "bob") {
+      answer = "Marley";
+    } else if (val === 42) {
+      answer = "The Answer";
+    } else if (val === 1) {
+      answer = "There is no #1";
+    } else if (val === 99) {
+      answer = "Missed me by this much!";
+    } else if (val === 7) {
+      answer = "Ate Nine";
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);  
   
   sequentialSizes(1);
