6b73aa343997f4add00d6b5cdf82132b5a65679a
Oksana <ksushatarasenko@gmail.com>
2023-12-13 06:30:59 +0100

    new

---
+++ 13_12_23.js
@@ -0,0 +1,15 @@
+const myDogs = {
+    name: 'Sharik',
+    legs: 4,
+    tails:60,
+    friends:['murzok', 'greg'],
+  };
+//   ===============
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+
+  myDog['name'] = 'Happy Coder'
